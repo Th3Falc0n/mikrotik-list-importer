64c8889ffc2b4b4f148868da792650888e3b76c5
Fabian Sachara <fabian@sachara.de>
2021-12-22 15:10:22 +0100

    bug fixes and optimization

--- server/src/main/scala/de/th3falc0n/mkts/backend/MikrotikConnection.scala
+++ server/src/main/scala/de/th3falc0n/mkts/backend/MikrotikConnection.scala
@@ -3,20 +3,20 @@ package de.th3falc0n.mkts.backend
 import de.th3falc0n.mkts.Main.config
 import de.th3falc0n.mkts.ip.IP
 import me.legrange.mikrotik.ApiConnection
-import org.slf4j.LoggerFactory
+import org.slf4j.{ Logger, LoggerFactory }
 
 import scala.jdk.CollectionConverters._
 
 object MikrotikConnection {
-  val logger = LoggerFactory.getLogger(this.getClass)
+  val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
-  logger.info("Connecting to API at {}", config.getString("mikrotik.host"))
-  val api = ApiConnection.connect(config.getString("mikrotik.host"))
+  def updateList(name: String, ips: Seq[IP]): Unit = {
+    logger.info("Connecting to API at {}", config.getString("mikrotik.host"))
+    val api = ApiConnection.connect(config.getString("mikrotik.host"))
 
-  logger.info("Logging in with user {}", config.getString("mikrotik.user"))
-  api.login(config.getString("mikrotik.user"), config.getString("mikrotik.password"))
+    logger.info("Logging in with user {}", config.getString("mikrotik.user"))
+    api.login(config.getString("mikrotik.user"), config.getString("mikrotik.password"))
 
-  def updateList(name: String, ips: Seq[IP]) = {
     logger.info("Updating list {}", name)
     val ipsAsString = ips.map(_.toString)
 
@@ -31,10 +31,14 @@ object MikrotikConnection {
     val toRemoveIds = toRemove.map(tr => result.find(_.get("address") == tr).get.get(".id"))
 
     if(toRemoveIds.nonEmpty) {
-      api.execute(s"/ip/firewall/address-list/remove numbers=${toRemoveIds.mkString(",")}")
+      toRemoveIds.grouped(100).foreach(ids => {
+        api.execute(s"/ip/firewall/address-list/remove numbers=${ids.mkString(",")}")
+      })
     }
 
     logger.info("Adding {} IPs to list {}", toAdd.length, name)
     toAdd.foreach(ip => api.execute(s"/ip/firewall/address-list/add list=$name address=\"$ip\""))
+
+    api.close()
   }
 }
--- server/src/main/scala/de/th3falc0n/mkts/lists/HttpIPSource.scala
+++ server/src/main/scala/de/th3falc0n/mkts/lists/HttpIPSource.scala
@@ -11,9 +11,7 @@ class HttpIPSource(val src: String) extends IPSource {
   private val uri = Uri(URI.create(src))
   private val logger = LoggerFactory.getLogger(s"iplist-$src")
 
-  private def range(i: Int, min: Int, max: Int) = i <= max && i >= min
-
-  def fetch = {
+  def fetch: Seq[IP] = {
     logger.info("Fetching", src)
     val request = basicRequest.get(uri)
 
@@ -25,15 +23,16 @@ class HttpIPSource(val src: String) extends IPSource {
       .split("\n")
       .filter(_.nonEmpty)
       .map(_.split(' ').head)
-      .filter(ip => "^[0-9][0123456789.\\/]*$".r.matches(ip))
-      .map {
-        case ip if ip.startsWith("10.") => "#"
-        case ip if ip.startsWith("172.") && range(ip.split('.').apply(1).toInt, 16, 31) => "#"
-        case ip if ip.startsWith("192.168.") => "#"
-        case ip => ip
+      .filter(ip => "^[0-9][0123456789./]*$".r.matches(ip))
+      .map(IP.fromString)
+      .filter {
+        case ip if IP.fromString("10.0.0.0/8").contains(ip) => false
+        case ip if IP.fromString("172.16.0.0/12").contains(ip) => false
+        case ip if IP.fromString("192.168.0.0/16").contains(ip) => false
+        case _ => true
       }
 
     logger.info("Got {} IPs", ips.length, src)
-    ips.map(IP.fromString)
+    ips
   }
 }
